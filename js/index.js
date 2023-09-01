359131d1d8ea93997dcfbc95af8e3f0c3f584fa8
damian-nestea <damian@hotmail.cz>
2023-09-01 09:12:48 -0300

    refactor creating de SPA structure with js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { Router } from "./routes.js";
+
+const router = new Router();
+
+router.add("/", "/pages/home.html");
+router.add("/media", "/pages/media.html");
+router.add("/contact", "/pages/contact.html");
+router.add(404, "/pages/404.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
