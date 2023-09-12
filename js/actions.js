a429494e6f9d357a1066b26b88bff9978f3f1b3f
damian-nestea <damian@hotmail.cz>
2023-09-12 11:10:24 -0300

    feat adding event to form button, checking fields and email address

--- js/actions.js
+++ js/actions.js
@@ -21,9 +21,39 @@ export function activateLink(pathname) {
         link.id == "contact-link"
           ? link.classList.add("active-link")
           : link.classList.remove("active-link");
+        let contactBtn = document.querySelector("button");
+        contactBtn.addEventListener("click", sendMessage);
       });
       break;
     default:
       return;
   }
+}
+
+function sendMessage(event) {
+  event.preventDefault();
+  const name = document.querySelector("#name");
+  const email = document.querySelector("#email");
+  const message = document.querySelector("#message");
+
+  if (!name.value || !email.value || !message.value) {
+    alert("Fill all the fields, please!");
+  } else {
+    if (isValidEmail(email.value)) {
+      alert("Thank you for sending this message");
+      name.value = "";
+      email.value = "";
+      message.value = "";
+    } else {
+      alert("Please enter a valid email");
+    }
+  }
+}
+
+function isValidEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
